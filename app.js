9fd2d9fe6879ba55d0f60ae2e6bfe4a2a7919d8b
Jordan McMillan <mcmij1@nationwide.com>
2018-07-20 03:48:13 -0400

    idk

--- app.js
+++ app.js
@@ -1,49 +1,97 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Include the cluster module
+var cluster = require('cluster');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Code to run if we're in the master process
+if (cluster.isMaster) {
 
-// Mongoose stuff
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-var mongoDB = 'mongodb://localhost:27017/flash-quotes';
-mongoose.connect(mongoDB, {useNewUrlParser: true});
-var db = mongoose.connection;
-db.on('error', function(err) {console.error('MongoDB connection error: ' + err);});
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-var app = express();
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    // Listen for terminating workers
+    cluster.on('exit', function (worker) {
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+        // Replace the terminated workers
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// Code to run if we're in a worker process
+} else {
+    var AWS = require('aws-sdk');
+    var express = require('express');
+    var bodyParser = require('body-parser');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    AWS.config.region = process.env.REGION
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    var sns = new AWS.SNS();
+    var ddb = new AWS.DynamoDB();
 
-module.exports = app;
+    var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+    var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
+    var app = express();
+
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+    app.use(bodyParser.urlencoded({extended:false}));
+
+    app.get('/', function(req, res) {
+        res.render('index', {
+            static_path: 'static',
+            theme: process.env.THEME || 'flatly',
+            flask_debug: process.env.FLASK_DEBUG || 'false'
+        });
+    });
+
+    app.post('/signup', function(req, res) {
+        var item = {
+            'email': {'S': req.body.email},
+            'name': {'S': req.body.name},
+            'preview': {'S': req.body.previewAccess},
+            'theme': {'S': req.body.theme}
+        };
+
+        ddb.putItem({
+            'TableName': ddbTable,
+            'Item': item,
+            'Expected': { email: { Exists: false } }        
+        }, function(err, data) {
+            if (err) {
+                var returnStatus = 500;
+
+                if (err.code === 'ConditionalCheckFailedException') {
+                    returnStatus = 409;
+                }
+
+                res.status(returnStatus).end();
+                console.log('DDB Error: ' + err);
+            } else {
+                sns.publish({
+                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
+                                        + "\r\nPreviewAccess: " + req.body.previewAccess 
+                                        + "\r\nTheme: " + req.body.theme,
+                    'Subject': 'New user sign up!!!',
+                    'TopicArn': snsTopic
+                }, function(err, data) {
+                    if (err) {
+                        res.status(500).end();
+                        console.log('SNS Error: ' + err);
+                    } else {
+                        res.status(201).end();
+                    }
+                });            
+            }
+        });
+    });
+
+    var port = process.env.PORT || 3000;
+
+    var server = app.listen(port, function () {
+        console.log('Server running at http://127.0.0.1:' + port + '/');
+    });
+}
